336862f243e945c0ef53bee557a37b9271be566a
paul-harness <56152762+paul-harness@users.noreply.github.com>
2021-01-07 14:42:12 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,14 @@
 terraform {
-  backend "s3" {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
   region = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
