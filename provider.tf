e441d40144720dadc2660c53267b4cf5c74f1fc8
paul-harness <56152762+paul-harness@users.noreply.github.com>
2021-01-08 08:56:03 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   region = var.region
-#   access_key = var.aws_access_key
-#   secret_key = var.aws_secret_key
-# }
+provider "aws" {
+  region = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
