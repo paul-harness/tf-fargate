38c56298788443da48b9af83d2d488a7b9baf7ef
paul-harness <56152762+paul-harness@users.noreply.github.com>
2021-01-08 08:46:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,24 +2,24 @@ variable "name" {
   default = "paul-tf-fargate"
 }
 
-variable "vpc_id" {
-  default = "vpc-0e7bedb2e71dd8025"
-}
+# variable "vpc_id" {
+#   default = "vpc-0e7bedb2e71dd8025"
+# }
 
-variable "subnet_id" {
-  default = "subnet-0d3650898e5d4ca9a"
-}
+# variable "subnet_id" {
+#   default = "subnet-0d3650898e5d4ca9a"
+# }
 
-variable "security_group_id" {
-  default = "sg-011b1f285fb3c9be1"
-}
+# variable "security_group_id" {
+#   default = "sg-011b1f285fb3c9be1"
+# }
 
-variable "region" {
-  default = "us-east-1"
-}
+# variable "region" {
+#   default = "us-east-1"
+# }
 
-variable "aws_access_key" {
-}
+# variable "aws_access_key" {
+# }
 
-variable "aws_secret_key" {
-}
+# variable "aws_secret_key" {
+# }
